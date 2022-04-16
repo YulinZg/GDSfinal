d5ecc7e7f80d5860f5926c3054917d9d611d2c79
YulinZg <lucienzhang0314@gmail.com>
2022-04-16 20:52:17 +0800

    fix bug

--- gdsfinal/Assets/Scripts/Map/RoomGenerator.cs
+++ gdsfinal/Assets/Scripts/Map/RoomGenerator.cs
@@ -39,13 +39,6 @@ public class RoomGenerator : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
-    }
 
     public void generateNormalRoom()
     {
