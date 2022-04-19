9da73247176fa230a13d13a3b39a7a9d8ab72392
OrangestarAlter <102082094+OrangestarAlter@users.noreply.github.com>
2022-04-20 03:40:34 +0800

    Skill UI

--- gdsfinal/Assets/Scripts/Player/Weapon.cs
+++ gdsfinal/Assets/Scripts/Player/Weapon.cs
@@ -13,8 +13,9 @@ public class Weapon : MonoBehaviour
         lightning,
         metal
     }
-
     public Property property;
+
+    [SerializeField] private SkillUI skillUI;
     [SerializeField] private float shootOffset;
     [SerializeField] private Transform fireRotater;
     [SerializeField] private Transform waterRotater;
@@ -124,6 +125,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] private float dropInterval;
     [SerializeField] private GameObject[] skillE;
     private float cooldownTimerE = 0;
+    private float skillTimerE = 0;
     private float dropTimer = 0;
     private bool isDroping = false;
 
@@ -195,26 +197,6 @@ public class Weapon : MonoBehaviour
     void Update()
     {
         shootTimer += Time.deltaTime;
-        if (!player.isHurting)
-            switch (property)
-            {
-                case Property.fire:
-                    Fire();
-                    break;
-                case Property.water:
-                    Water();
-                    break;
-                case Property.earth:
-                    Earth();
-                    break;
-                case Property.lightning:
-                    Lightning();
-                    break;
-                case Property.metal:
-                    Metal();
-                    break;
-            }
-        RotateBullet(bulletSpeedF * 10);
         if (cooldownTimerF != 0)
         {
             cooldownTimerF -= Time.deltaTime;
@@ -234,7 +216,16 @@ public class Weapon : MonoBehaviour
                 cooldownTimerE = 0;
         }
         if (isDroping)
+        {
+            skillTimerE -= Time.deltaTime;
             Drop();
+            if (skillTimerE <= 0)
+            {
+                skillTimerE = 0;
+                isDroping = false;
+                cooldownTimerE = cooldownTimeE;
+            }
+        }
         if (cooldownTimerL != 0)
         {
             cooldownTimerL -= Time.deltaTime;
@@ -246,6 +237,7 @@ public class Weapon : MonoBehaviour
             skillTimerL -= Time.deltaTime;
             if (skillTimerL <= 0)
             {
+                skillTimerL = 0;
                 hasMoved = false;
                 Destroy(backPos.gameObject);
                 cooldownTimerL = cooldownTimeL;
@@ -257,6 +249,43 @@ public class Weapon : MonoBehaviour
             if (cooldownTimerM < 0)
                 cooldownTimerM = 0;
         }
+        switch (property)
+        {
+            case Property.fire:
+                if (!player.isHurting)
+                    Fire();
+                skillUI.SetCooldownFill(cooldownTimerF / cooldownTimeF);
+                break;
+            case Property.water:
+                if (!player.isHurting)
+                    Water();
+                skillUI.SetCooldownFill(cooldownTimerW / cooldownTimeW);
+                break;
+            case Property.earth:
+                if (!player.isHurting)
+                    Earth();
+                if (!isDroping)
+                    skillUI.SetCooldownFill(cooldownTimerE / cooldownTimeE);
+                else
+                    skillUI.SetCooldownFill(1);
+                skillUI.SetSkillingFill(skillTimerE / skillTimeE);
+                break;
+            case Property.lightning:
+                if (!player.isHurting)
+                    Lightning();
+                if (!hasMoved)
+                    skillUI.SetCooldownFill(cooldownTimerL / cooldownTimeL);
+                else
+                    skillUI.SetCooldownFill(1);
+                skillUI.SetSkillingFill(skillTimerL / skillTimeL);
+                break;
+            case Property.metal:
+                if (!player.isHurting)
+                    Metal();
+                skillUI.SetCooldownFill(cooldownTimerM / cooldownTimeM);
+                break;
+        }
+        RotateBullet(bulletSpeedF * 10);
     }
 
     public void GetWeapon(Property type)
@@ -268,6 +297,7 @@ public class Weapon : MonoBehaviour
             case Property.fire:
                 player.SetSpeed(moveSpeedF);
                 canLaunch = true;
+                skillUI.SetSkillingFill(0);
                 break;
             case Property.water:
                 player.SetSpeed(moveSpeedW);
@@ -280,6 +310,7 @@ public class Weapon : MonoBehaviour
                 spawnedEffect1 = false;
                 spawnedEffect2 = false;
                 spawned2Effect2 = false;
+                skillUI.SetSkillingFill(0);
                 break;
             case Property.earth:
                 player.SetSpeed(moveSpeedE);
@@ -302,6 +333,7 @@ public class Weapon : MonoBehaviour
                 leftDown = false;
                 rightDown = false;
                 lastClick = 0;
+                skillUI.SetSkillingFill(0);
                 break;
         }
         if (bulletsInWorld.childCount > 0)
@@ -737,13 +769,7 @@ public class Weapon : MonoBehaviour
     {
         isDroping = true;
         dropTimer = 0;
-        Invoke(nameof(StopDrop), skillTimeE);
-    }
-
-    private void StopDrop()
-    {
-        isDroping = false;
-        cooldownTimerE = cooldownTimeE;
+        skillTimerE = skillTimeE;
     }
 
     private void Drop()
@@ -840,6 +866,7 @@ public class Weapon : MonoBehaviour
             hasMoved = false;
             Destroy(backPos.gameObject);
             cooldownTimerL = cooldownTimeL;
+            skillTimerL = 0;
         }
     }
 
